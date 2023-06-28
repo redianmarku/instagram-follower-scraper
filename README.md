458def869ea060d7073c4e8b66938d3a4de6405a
Redian Marku <42848358+redianmarku@users.noreply.github.com>
2023-06-29 00:43:21 +0200

    Fixed bugs, also now you can scrape from multiple accounts

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
-# instagram-follower-scraper
+# Instagram Follower Scraper
 
-This python script can automatically scrape other Instagram users' followers and save them in a text file.
-Fixed and updated version so it works at today
+This Python script allows you to automatically scrape the followers of multiple Instagram users and save them in separate text files. It utilizes Selenium WebDriver to interact with the Instagram website.
 
-# How to use:
+## Requirements
 
-1. Install requirements using terminal or command line. You should run this command `pip install -r requirements.txt`.
-2. Rename .env.example to .env and add ig username and password as in example file.
-3. Open a terminal or cmd again and run the bot using this command: `python run.py`.
-4. Enter the username of the person whose followers you want to scrape.
-5. Enter how many followers you want to scrape.
-6. And that's it. After a while, you will find the list of usernames in a text file in the bot folder.
+Python 3.x
+Selenium
+ChromeDriver
+Install the required packages by running the following command in your terminal or command line:
+
+`pip install -r requirements.txt`
+
+## Credentials
+
+-Run the script using the command: `python run.py`.
+-Enter your Instagram username and password when prompted.
+-The credentials will be saved in the specified file for future use.
+
+## Instagram Usernames
+
+-Run the script using the command: `python run.py.`
+-Enter the Instagram usernames you want to scrape, separated by commas.
+-Example: username1, username2, username3
+-The followers' usernames will be saved in separate text files, named as {username}\_followers.txt, for each user.
+
+Please note that scraping a large number of followers in a short period of time may be subject to limitations and restrictions imposed by Instagram. Ensure that you use this script responsibly and within the bounds of Instagram's terms of service.
+
+If you encounter any issues or have any questions, feel free to reach out or create an issue in this repository.
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-import time, os
+import time
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,20 +10,95 @@ from webdriver_manager.chrome import ChromeDriverManager as CM
 from selenium.common.exceptions import NoSuchElementException
 
 
-USERNAME = os.environ.get('USERNAME')
-PASSWORD = os.environ.get('PASSWORD')
+def save_credentials(username, password):
+    with open('credentials.txt', 'w') as file:
+        file.write(f"{username}\n{password}")
 
-TIMEOUT = 15
+
+def load_credentials():
+    if not os.path.exists('credentials.txt'):
+        return None
+
+    with open('credentials.txt', 'r') as file:
+        lines = file.readlines()
+        if len(lines) >= 2:
+            return lines[0].strip(), lines[1].strip()
+
+    return None
+
+
+def prompt_credentials():
+    username = input("Enter your Instagram username: ")
+    password = input("Enter your Instagram password: ")
+    save_credentials(username, password)
+    return username, password
+
+
+def login(bot, username, password):
+    bot.get('https://www.instagram.com/accounts/login/')
+    time.sleep(1)
+
+    # Check if cookies need to be accepted
+    try:
+        element = bot.find_element(By.XPATH, "/html/body/div[4]/div/div/div[3]/div[2]/button")
+        element.click()
+    except NoSuchElementException:
+        print("[Info] - Instagram did not require to accept cookies this time.")
+
+    print("[Info] - Logging in...")
+    username_input = WebDriverWait(bot, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+    password_input = WebDriverWait(bot, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+
+    username_input.clear()
+    username_input.send_keys(username)
+    password_input.clear()
+    password_input.send_keys(password)
+
+    login_button = WebDriverWait(bot, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
+    login_button.click()
+    time.sleep(10)
+
+
+def scrape_followers(bot, username, user_input):
+    bot.get(f'https://www.instagram.com/{username}/')
+    time.sleep(3.5)
+    WebDriverWait(bot, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, "//a[contains(@href, '/followers')]"))).click()
+    time.sleep(2)
+    print(f"[Info] - Scraping followers for {username}...")
+
+    users = set()
+
+    for _ in range(round(user_input // 20)):
+        ActionChains(bot).send_keys(Keys.END).perform()
+        time.sleep(1)
+
+    followers = bot.find_elements(By.XPATH, "//a[contains(@href, '/')]")
+
+    for i in followers:
+        if i.get_attribute('href'):
+            users.add(i.get_attribute('href').split("/")[3])
+        else:
+            continue
+
+    print(f"[Info] - Saving followers for {username}...")
+    with open(f'{username}_followers.txt', 'a') as file:
+        file.write('\n'.join(users) + "\n")
 
 
 def scrape():
-    usr = input('[Required] - Whose followers do you want to scrape: ')
+    credentials = load_credentials()
 
-    user_input = int(
-        input('[Required] - How many followers do you want to scrape (60-500 recommended): '))
+    if credentials is None:
+        username, password = prompt_credentials()
+    else:
+        username, password = credentials
+
+    user_input = int(input('[Required] - How many followers do you want to scrape (60-500 recommended): '))
+
+    usernames = input("Enter the Instagram usernames you want to scrape (separated by commas): ").split(",")
 
     options = webdriver.ChromeOptions()
-    #options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument('--no-sandbox')
     options.add_argument("--log-level=3")
     mobile_emulation = {
@@ -31,82 +107,15 @@ def scrape():
 
     bot = webdriver.Chrome(executable_path=CM().install(), options=options)
 
-    bot.get('https://www.instagram.com/accounts/login/')
+    login(bot, username, password)
 
-    time.sleep(1)
+    for user in usernames:
+        user = user.strip()
+        scrape_followers(bot, user, user_input)
 
-
-    #check if cookies 
-    try:
-        element = bot.find_element(By.XPATH,"/html/body/div[4]/div/div/div[3]/div[2]/button")
-        element.click()
-        
-    except NoSuchElementException:
-        print("[Info] - Instagram did not require to accept cookies this time.")
-
-
-        
-
-    print("[Info] - Logging in...")  
-
-    username = WebDriverWait(
-        bot, 10).until(EC.element_to_be_clickable(
-        (By.CSS_SELECTOR, "input[name='username']")))
-
-    # target Password
-    password = WebDriverWait(
-        bot, 10).until(EC.element_to_be_clickable(
-        (By.CSS_SELECTOR, "input[name='password']")))
-
-    # enter username and password
-    username.clear()
-    username.send_keys(USERNAME)
-    password.clear()
-    password.send_keys(PASSWORD)
-
-    # target the login button and click it
-    button = WebDriverWait(
-        bot, 2).until(EC.element_to_be_clickable(
-        (By.CSS_SELECTOR, "button[type='submit']"))).click()
-
-    time.sleep(10)
-
-    bot.get('https://www.instagram.com/{}/'.format(usr))
-
-    time.sleep(3.5)
-
-    WebDriverWait(bot, TIMEOUT).until(
-        EC.presence_of_element_located((
-            By.XPATH, "//a[contains(@href, '/following')]"))).click()
-
-    time.sleep(2)
-
-    print('[Info] - Scraping...')
-
-    users = set()
-
-    for _ in range(round(user_input // 20)):
-
-        ActionChains(bot).send_keys(Keys.END).perform()
-
-        time.sleep(1)
-
-    followers = bot.find_elements(By.XPATH,
-    "//a[contains(@href, '/')]")
-
-    # Getting url from href attribute
-    for i in followers:
-        if i.get_attribute('href'):
-            users.add(i.get_attribute('href').split("/")[3])
-        else:
-            continue
-
-    print('[Info] - Saving...')
-    print('[DONE] - Your followers are saved in followers.txt file!')
-
-    with open('followers.txt', 'a') as file:
-        file.write('\n'.join(users) + "\n")
+    bot.quit()
 
 
 if __name__ == '__main__':
+    TIMEOUT = 15
     scrape()
